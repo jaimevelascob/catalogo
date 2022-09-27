87fd9b43a4d91b3ad8f8dfaed1afa5f8be3bf62b
AdrianBarbeito <adrianbarbeitoruiz@gmail.com>
2022-09-27 16:13:59 +0200

    Models de la base de datos

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
+from unicodedata import name
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+class Rols(models.Model):
+    rols = models.CharField(max_length=20, null = False, default = None)
+
+class User(models.Model):
+    name = models.CharField(max_length=50, null = False, default = None)
+    username = models.CharField(max_length=20, null = False, default = None)
+    password = models.CharField(null = False, default = None)
+    rol_id = models.ForeignKey(Rols, on_delete=models.CASCADE, null = False, default = None)
+
+class Article(models.Model):
+    content = models.TextField(null = False, default = None)
+    photo = models.CharField(max_length=20, null = False, default = None)
+
+class Comments(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, null = False, default = None)
+    user = models.CharField(max_length = 20, null = False, default = None)
+    comment = models.TextField(null = False, default = None)
