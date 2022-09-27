cf708c8d01e752ac61db018b656ec5bedd053a7c
jaimevelascob <jaimevelasco3@hotmail.com>
2022-09-27 13:51:40 +0200

    template

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CatalogoConfig(AppConfig):
+class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'catalogo'
+    name = 'main'
