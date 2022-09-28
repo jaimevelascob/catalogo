3784a3d2d5edbee8570a606fb397bfe3e2c8b942
AdrianBarbeito <adrianbarbeitoruiz@gmail.com>
2022-09-28 11:23:02 +0200

    Views con los artículos y los comentarios cargados

--- main/views.py
+++ main/views.py
@@ -5,8 +5,11 @@ from main.models import User, Article, Comments
 
 
 def index(request):
-	articles = Article.objects.all()
-	context = {
-		'articles' : articles,
-	}
-	return render(request, '', context)
+    articles = Article.objects.all()
+    comments = Comments.objects.all()
+
+    context = {
+        'articles' : articles,
+        'comments' : comments
+    }
+    return render(request, 'main.html', context)
