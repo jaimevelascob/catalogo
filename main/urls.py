b86020849197a9e95922316132b5d49c5ae3837d
omuculaptele <florin.moldoveanu.ceb@immune.institute>
2022-09-28 16:15:39 +0200

    articulo

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('signin', views.signin, name='signin'),
     path('<int:pk>', views.news, name='news'),
     path('aboutus', views.aboutus, name='aboutus'),
-    path('subscribe', views.subscribe, name='subscribe'),   
+    path('subscribe', views.subscribe, name='subscribe'),
+    path('article', views.article, name='article'),  
 ]
--- main/views.py
+++ main/views.py
@@ -6,6 +6,7 @@ from main.models import User, Article, Comments
 
 def index(request):
     return HttpResponse("Hola soy Index")
+    
 
 def signup(request):
     return HttpResponse("Hola soy Signup")
@@ -21,3 +22,12 @@ def subscribe(request):
 
 def news(request):
     return HttpResponse("Hola soy News")
+
+
+def article(request):
+	article = Article.objects.all()
+	context = {
+		'article' : article,
+	}
+	return render(request, 'article.html', context)
+	
