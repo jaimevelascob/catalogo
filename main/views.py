94fa9eab2993d21332603dc252151e91b85be21b
AdrianBarbeito <adrianbarbeitoruiz@gmail.com>
2022-09-28 20:38:39 +0200

    Froms creado e introducido en el views

--- main/views.py
+++ main/views.py
@@ -2,31 +2,42 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from main.models import User, Article, Comments
+from main.forms import signUpForm
 
 
 def main(request):
     return render(request, 'main.html')
-    
+
 
 def signup(request):
     return HttpResponse("Hola soy Signup")
 
+
 def signin(request):
     return HttpResponse("Hola soy Signin")
+
 
 def aboutus(request):
     return HttpResponse("Hola soy Aboutus")
 
+
 def subscribe(request):
     return HttpResponse("Hola soy Subscribe")
+
 
 def news(request):
     return HttpResponse("Hola soy News")
 
+
 def article(request):
-	article = Article.objects.all()
-	context = {
-		'article' : article,
-	}
-	return render(request, 'article.html', context)
-	
+    article = Article.objects.all()
+    comments = Comments.objects.all()
+    form = signUpForm()
+
+    context = {
+        'article': article,
+        'comments': comments,
+        'form': form,
+
+    }
+    return render(request, 'article.html', context)
